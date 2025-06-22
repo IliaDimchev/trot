f70dced36a0d03339dbd9711fe1b52b4ec151a25
IliaDimchev <dimchev.ilia@gmail.com>
2025-06-22 23:30:34 +0100

    Revert "printing the verify_recaptcha result"
    This reverts commit 167b36a34a798dd188a5a7ac294304459c94afa1.

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def home():
     recaptcha_token = request.form.get("g-recaptcha-response")
     if not verify_recaptcha(recaptcha_token):
         flash("Моля, потвърдете, че не сте робот.")
-        print(verify_recaptcha(recaptcha_token))
+        print('verification failed')
 
     if request.method == "POST":
         if request.form.get("website"):  # honeypot
