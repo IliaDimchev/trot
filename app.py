3d669bf545c1b04729a4e1eeb1cea1330ed087ca
ilkoTheTiger <ilkothetiger@gmail.com>
2025-10-17 22:53:35 +0300

    timestamp added to ServiceRequest model

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ class ServiceRequest(db.Model):
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(20), nullable=True)
     message = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.String(30), nullable=True)
 
 # Потребител за админ
 class Admin(UserMixin):
